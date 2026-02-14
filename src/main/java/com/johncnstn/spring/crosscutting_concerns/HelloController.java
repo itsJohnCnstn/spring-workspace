3db9403ee31cecdabbf81a2fe6ecbebb09a93a15
Pavel Khankevich <johncnstn@gmail.com>
2026-02-14 17:17:28 +0100

    [crosscutting_concerns] implement api

---
+++ src/main/java/com/johncnstn/spring/crosscutting_concerns/HelloController.java
@@ -0,0 +1,22 @@
+package com.johncnstn.spring.crosscutting_concerns;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    /*
+        http localhost:8080/hello
+
+        or
+
+        curl -X GET --location "http://localhost:8080/hello" \
+        -H "Accept: application/json"
+     */
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hey there!";
+    }
+
+}
