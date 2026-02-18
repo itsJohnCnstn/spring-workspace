72890be17269e8b71df24680bb258d3d7bdc4b29
Pavel Khankevich <johncnstn@gmail.com>
2026-02-18 14:28:47 +0100

    [jpa/questions/findByEmail] add answer comment: SELECT * FROM users WHERE email = ?

--- src/main/java/com/johncnstn/spring/jpa/PersonRepository.java
+++ src/main/java/com/johncnstn/spring/jpa/PersonRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<User, Long> {
+    // SELECT * FROM users WHERE email = ?
     Optional<User> findByEmail(String email);
 }
