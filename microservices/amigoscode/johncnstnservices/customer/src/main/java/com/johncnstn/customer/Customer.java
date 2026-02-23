c7ca4551c08e7195e6b2b0b962c0e4708344f933
Pavel Khankevich <johncnstn@gmail.com>
2026-02-23 17:15:11 +0100

    [microservices/amigoscode/customer_service_setup] complete create customer flow. add API.http

--- microservices/amigoscode/johncnstnservices/customer/src/main/java/com/johncnstn/customer/Customer.java
+++ microservices/amigoscode/johncnstnservices/customer/src/main/java/com/johncnstn/customer/Customer.java
@@ -1,13 +1,50 @@
 package com.johncnstn.customer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
     private String firstName;
     private String lastName;
     private String email;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Customer customer = (Customer) o;
+        return getId() != null && Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- microservices/amigoscode/johncnstnservices/customer/src/main/java/com/johncnstn/customer/CustomerService.java
+++ microservices/amigoscode/johncnstnservices/customer/src/main/java/com/johncnstn/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.johncnstn.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
@@ -12,6 +12,6 @@ public record CustomerService() {
                 .build();
         //todo: check if email valid
         //todo: check if email not taken
-        //todo: store customer in db
+        customerRepository.save(customer);
     }
 }
