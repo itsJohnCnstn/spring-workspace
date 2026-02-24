9f68a0e96a961b0f468d51540b79d5fd051194be
Pavel Khankevich <johncnstn@gmail.com>
2026-02-24 12:02:10 +0100

    Microservices/amigoscode/fraud (#11)
    * [microservices/amigoscode/fraud] setup fraud service
    
    * [microservices/amigoscode/fraud] add n-layers for fraud service
    
    * [microservices/amigoscode/fraud] add restTemplate for customer
    
    * [microservices/amigoscode/fraud] add http communication between services via resttemplate

---
+++ microservices/amigoscode/johncnstnservices/customer/src/main/java/com/johncnstn/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.johncnstn.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ microservices/amigoscode/johncnstnservices/fraud/src/main/java/com/johncnstn/fraud/FraudApplication.java
@@ -0,0 +1,11 @@
+package com.johncnstn.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApplication {
+    static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
