8609b406b25d734b24f26c1d120b227b951678ed
Pavel Khankevich <johncnstn@gmail.com>
2026-02-25 16:38:31 +0100

    [microservices/amigoscode/kafka] add topic config

---
+++ microservices/amigoscode/kafka-example/src/main/java/com/johncnstn/kafkaexample/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package com.johncnstn.kafkaexample.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic johncnstnTopic() {
+        return TopicBuilder.name("johncnstn")
+                .build();
+    }
+
+}
