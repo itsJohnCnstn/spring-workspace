5055721ee0a02a913684f4cc44dac415a4f972ea
Pavel Khankevich <johncnstn@gmail.com>
2026-03-03 07:10:36 +0100

    [rabbit/spring/getting_started] init

---
+++ rabbit/spring-rabbitmq-gettingstarted/src/test/java/com/johncnstn/springgettingstarted/SpringGettingstartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johncnstn.springgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGettingstartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
