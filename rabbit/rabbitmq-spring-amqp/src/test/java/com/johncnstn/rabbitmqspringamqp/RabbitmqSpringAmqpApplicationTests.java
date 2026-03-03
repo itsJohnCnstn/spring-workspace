b5c847479e316833ef3088a59c7be962fb821a34
Pavel Khankevich <johncnstn@gmail.com>
2026-03-03 18:35:30 +0100

    Rabbit/rabbitmq spring amqp/tutorial one (#16)
    * [rabbit/rabbitmq-spring-amqp/tutorial-one] init
    
    * [rabbit/rabbitmq-spring-amqp/tutorial-one] implement consumer receiver via  named queue

---
+++ rabbit/rabbitmq-spring-amqp/src/test/java/com/johncnstn/rabbitmqspringamqp/RabbitmqSpringAmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johncnstn.rabbitmqspringamqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringAmqpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
