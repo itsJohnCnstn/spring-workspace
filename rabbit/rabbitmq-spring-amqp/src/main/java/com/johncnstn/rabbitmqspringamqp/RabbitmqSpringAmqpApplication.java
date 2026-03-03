5fc3cff820d79e292b837168f2416e54ee90e55d
Pavel Khankevich <johncnstn@gmail.com>
2026-03-03 13:45:03 +0100

    [rabbit/rabbitmq-spring-amqp/tutorial-one] init

---
+++ rabbit/rabbitmq-spring-amqp/src/main/java/com/johncnstn/rabbitmqspringamqp/RabbitmqSpringAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.johncnstn.rabbitmqspringamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringAmqpApplication.class, args);
+    }
+
+}
